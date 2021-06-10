5c9e5ee105f52bba3f85b30662b5dc98c83c6819
Matthias Kohler <matthias.c.kohler@gmail.com>
2021-06-10 12:47:18 +0000

    Set up more react stuff, including routing
    Still need 404 stuff to work on gh pages

--- src/index.js
+++ src/index.js
@@ -11,6 +11,20 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// TODOM: Add 404 routing: 
+//         https://stackoverflow.com/questions/46056414/getting-404-for-links-with-create-react-app-deployed-to-github-pages
+//         also https://create-react-app.dev/docs/deployment/#notes-on-client-side-routing
+
+// TODOM: Make a basic blog
+
+// TODOM: Link between pages
+
+// TODOM: Photos?
+
+// TODOM: Page title, favicon, logo
+
+// TODOM: Do I want react helmet?
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
