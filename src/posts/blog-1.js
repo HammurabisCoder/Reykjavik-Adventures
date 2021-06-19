f31a9fcf742e4139b7b8077f935aa2f337308d08
Matthias Kohler <matthias.c.kohler@gmail.com>
2021-06-19 12:55:23 +0000

    Add image to blog

--- src/posts/blog-1.js
+++ src/posts/blog-1.js
@@ -1,3 +1,5 @@
+import laptop from '../assets/june-19-laptop.jpg';
+
 export default `# Perlan Revisited
 
 #### June 19, 2021 by Matthias
@@ -6,4 +8,6 @@ Finally, something resembling a blog. I'm at Perlan again, on the top slowly rot
 I just had a scoop of chocolate ice cream and am sipping on some mango tea as I type this.
 The parents arrived yesterday, and they're going through the museum now while I wait for them up here.
 This blog/website is super jank, but it's a start. Now to start collecting and backfilling my old scattered notes from
-text files, Facebook posts, and my notepad. `;
+text files, Facebook posts, and my notepad.
+
+![laptop](${laptop})`;
